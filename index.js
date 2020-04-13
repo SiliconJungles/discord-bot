a29daf0de738c6f71bc39ce4da0a25e8f0edb129
yaojie <limyaojie93@gmail.com>
2020-04-13 18:13:51 +0800

    fix split to use whitespace instead of only space

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-client.on("message", msg => {
-  msg.content.split(" ").forEach(item => {
+client.on("message", (msg) => {
+  msg.content.split(/\s/).forEach((item) => {
     if (isUrl(item)) {
+      console.log(`Submitting ${item}`);
       postToServer(item, msg.author.username);
     }
   });
