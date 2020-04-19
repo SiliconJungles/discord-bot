66315c205211f628c8a2107eae5187b042164deb
yaojie <limyaojie93@gmail.com>
2020-04-19 20:30:08 +0800

    add cron job for show and tell

--- index.js
+++ index.js
@@ -5,16 +5,29 @@ require("dotenv").config();
 const isUrl = require("./src/isUrl");
 const postToServer = require("./src/postToServer");
 const Discord = require("discord.js");
+const CronJob = require('cron').CronJob;
 
+const SHOW_N_TELL_CHANNEL = 692926834525995108;
 const client = new Discord.Client();
 
 client.on("message", (msg) => {
-  msg.content.split(/\s/).forEach((item) => {
-    if (isUrl(item)) {
-      console.log(`Submitting ${item}`);
-      postToServer(item, msg.author.username);
-    }
-  });
+  if (msg.channel.id !== SHOW_N_TELL_CHANNEL) {
+    msg.content.split(/\s/).forEach((item) => {
+      if (isUrl(item)) {
+        console.log(`Submitting ${item}`);
+        postToServer(item, msg.author.username);
+      }
+    });
+  }
 });
 
 client.login(process.env.TOKEN);
+
+const notifyUsersForShowAndTell = new CronJob('55 17 * * 1-5', () => {
+  console.log(new Date());
+  client.channels.fetch(SHOW_N_TELL_CHANNEL).then(channel => channel.send(
+    "@everyone, Get ready for Show and Tell!"
+  ));
+}, null, false, 'Asia/Singapore');
+
+notifyUsersForShowAndTell.start();
