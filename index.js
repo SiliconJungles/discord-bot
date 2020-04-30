c3e4e0a619c2f0c0df1ae6b8ccc4f3b1f0ab96d1
yaojie <limyaojie93@gmail.com>
2020-04-30 09:42:30 +0800

    add holiday parsing for show and tell

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const isUrl = require("./src/isUrl");
 const postToServer = require("./src/postToServer");
 const Discord = require("discord.js");
 const CronJob = require('cron').CronJob;
+const getHolidays = require('public-holidays').getHolidays;
 
 const SHOW_N_TELL_CHANNEL = '692926834525995108';
 const client = new Discord.Client();
@@ -24,10 +25,22 @@ client.on("message", (msg) => {
 client.login(process.env.TOKEN);
 
 const notifyUsersForShowAndTell = new CronJob('45 17 * * 1-5', () => {
-  console.log(new Date());
-  client.channels.fetch(SHOW_N_TELL_CHANNEL).then(channel => channel.send(
-    "@everyone, Get ready for Show and Tell!"
-  ));
+  getHolidays({ country: 'sg', lang: 'en' }).then((holidays) => {
+    const holidayDates = {}
+    holidays.forEach(holiday => holidayDates[holiday["date"].toLocaleDateString("en-SG")] = holiday["name"])
+
+    const today = new Date().toLocaleDateString("en-SG")
+    if (holidayDates[today]) {
+      console.log(`Today is a holiday! ${holidayDates[today]}`)
+    } else {
+      console.log(`Starting show and tell: ${today}`)
+      client.channels.fetch(SHOW_N_TELL_CHANNEL).then(channel => channel.send(
+        "@everyone, Get ready for Show and Tell!"
+      ));
+    }
+  }).catch((error) => {
+    console.error(error)
+  })
 }, null, false, 'Asia/Singapore');
 
 notifyUsersForShowAndTell.start();
